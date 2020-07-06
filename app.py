05e89d219aef98e774f36cfe5cfa01478bbde1e2
chenhong <chenhong@dianchu.cc>
2020-07-06 21:09:24 +0800

    add ignore

--- app.py
+++ app.py
@@ -17,13 +17,18 @@ def hello():
 def user_page(name):
     return "User: %s" % escape(name)
 
+
 @app.route("/AynRand/")
 def hello_aynrand():
-    return "<h1>Hello Ayn Rand!</h1><img src='https://static.ffx.io/images/$zoom_0.2015%2C$multiply_2.1164%2C$ratio_1%2C$width_378%2C$x_0%2C$y_67/t_crop_custom/e_sharpen:25%2Cq_85%2Cf_auto/1ed9787e5cb2615285bd3f969531a00096de1775'>"
+    return "<h1>Hello Ayn Rand!</h1><img src='https://static.ffx.io/images/$zoom_0.2015%2C$multiply_2.1164%2C$" \
+           "ratio_1%2C$width_378%2C$x_0%2C$y_67/t_crop_custom/e_sharpen:25%2Cq_85%2Cf_auto/" \
+           "1ed9787e5cb2615285bd3f969531a00096de1775'>"
+
 
 @app.route("/Totoro/")
 def hello_totoro():
     return "<h1>Hello Totoro!</h1><img src='http://helloflask.com/totoro.gif'>"
+
 
 @app.route('/test')
 def test_url_for():
@@ -36,6 +41,7 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
 
 if __name__ == "__man__":
     app.run()
